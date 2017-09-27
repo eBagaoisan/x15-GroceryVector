58d296101560f29cb3ce9a8b6240cec8634b0058
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-09-27 16:52:41 -0700

    Added show list and quit to program

--- groceryList.cpp
+++ groceryList.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main(){
     string item;
+    int remove;
     char input;
     vector<string> grocery;
     bool cond = true;
@@ -17,17 +18,33 @@ int main(){
         cout<<"\nEnter your choice: ";
         cout<<"\n(A)dd an item";
         cout<<"\n(S)how grocery list";
-        cout<<"\n(R)emove an item";
         cout<<"\n(Q)uit";
-        cout<<"\nYour choice(A/S/R/Q): ";
+        cout<<"\nYour choice(A/S/Q): ";
         cin>>input;
-
+        
+        //Add
         if(input == 'a' || input == 'A'){
-            //grocery.resize(grocery.size()+1);
+            grocery.resize(grocery.size()+1);
             cout<<"Add Items to your cart: ";
             cin>>item;
-           // grocery.insert(grocery.end(), item);
+            grocery.insert(grocery.end(), item);
             cout<<endl;
         }
+        //List
+        else if(input == 's' || input == 'S'){
+            for(int i=0; i<grocery.size(); i++){
+                cout<<grocery[i]<<endl;
+            }
+        }
+        //Quit
+        else if(input == 'q' || input == 'Q'){
+            cond = false;
+        }
+        
+        else{
+            cout<< "Invalid Entry, try again..."<<endl;
+        }
     }
+    return 0;
 }
+
